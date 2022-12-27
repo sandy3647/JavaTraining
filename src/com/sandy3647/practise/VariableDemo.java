27ea61eb522cb1a9026c11d43fc0791fed9d579c
sandy3647 <sandy3647@gmail.com>
2022-12-27 23:21:15 +0100

    Java training

---
+++ src/com/sandy3647/practise/VariableDemo.java
@@ -0,0 +1,27 @@
+package com.sandy3647.practise;
+
+public class VariableDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//by default decimal values are double so we add f in the end
+		System.out.println("HelloWorld");
+		long j = 5000l;
+		int i = 5; // int takes 4 bytes		
+		short s = 5; //short takes 2 bytes
+		byte k = 5; //1 byte
+		char c = 'A';
+		
+		c = 66;
+		System.out.println("**** C + " + c);
+		
+		double d = 5; //implicit conversion
+		
+		int k1 = (int) 5.6;
+		
+		System.out.println("++ Values of int K1" + k1);
+		
+
+	}
+
+}
